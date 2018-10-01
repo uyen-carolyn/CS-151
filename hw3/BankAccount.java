81cad7a8e5ec9b533325f4b66806ae4359f3ece9
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-09-30 21:11:12 -0700

    Create BankAccount.java

---
+++ hw3/BankAccount.java
@@ -0,0 +1,31 @@
+import java.util.*; 
+/**
+ * BankAccount class used by Comparator (see CompareByBalance)
+ * @author Uyen Nguyen
+ *
+ */
+public class BankAccount{
+	private String name; 
+	private double balance; 
+	
+	public BankAccount(String name, double balance) {
+		this.name = name; 
+		this.balance = balance; 
+	}
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public double getBalance() {
+		return balance; 
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance; 
+	}
+}
