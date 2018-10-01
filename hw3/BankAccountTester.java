553f58d77b29872e46607b6afa99aa112dadd734
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-09-30 21:12:47 -0700

    Update BankAccountTester.java

--- hw3/BankAccountTester.java
+++ hw3/BankAccountTester.java
@@ -13,13 +13,16 @@ public class BankAccountTester
      public static void main(String[] args)
      {
         ArrayList<BankAccount> account = new ArrayList<>();
+        
         account.add(new BankAccount("a", 1345.29));
         account.add(new BankAccount("b", 2499.31));
         account.add(new BankAccount("c", 2480.00));
         account.add(new BankAccount("d", 732.56));
         account.add(new BankAccount("e", 3345.44));
         account.add(new BankAccount("f", 800.25));
+        
         Comparator<BankAccount>  comp = new CompareByBalance();
+        
         Collections.sort(account, comp);
           
         for (BankAccount a : account)
