d6f8c83948f93f4a24696cbc0a75968c8f8cd146
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-10-14 00:27:17 -0700

    Update Tools.java

--- In Class Assignments/Tools.java
+++ In Class Assignments/Tools.java
@@ -6,56 +6,44 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Tools extends JPanel {
-
-private JButton Exit, Reset;
-private Board Brd;
-private JLabel Player1Wins, Player2Wins;
-
-
-Tools() {
+	private JButton Exit, Reset;
+	private Board Brd;
+	private JLabel Player1Wins, Player2Wins;
 	
-	setLayout(new GridLayout(1,2));
-    Player1Wins = new JLabel("Player 1 Wins:  0");
+	Tools() {
+		setLayout(new GridLayout(1,2));
+    		Player1Wins = new JLabel("Player 1 Wins:  0");
 	
-    Player2Wins = new JLabel("Player 2 Wins:  0");
+    		Player2Wins = new JLabel("Player 2 Wins:  0");
 	
-    setLayout(new FlowLayout());
+    		setLayout(new FlowLayout());
 
-    Exit = new JButton("Exit");
-    Reset = new JButton("Reset");
+    		Exit = new JButton("Exit");
+    		Reset = new JButton("Reset");
     
+    		Exit.addActionListener(new ActionListener() {
 
-    Exit.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent ae) {
+            		System.exit(0);
+        	}
+    	});
+    		Reset.addActionListener(new ActionListener() {
+        	
+		public void actionPerformed(ActionEvent ae) {
+            		Brd.Reset();
+            		Player1Wins.setText("Player 1 Wins: "+ Brd.getPlayer1Score());
+            		Player2Wins.setText("Player 2 Wins: "+ Brd.getPlayer2Score());
+        	}
+    	});
 
-        public void actionPerformed(ActionEvent ae) {
-            System.exit(0);
-        }
-    });
-
-    Reset.addActionListener(new ActionListener() {
-
-        public void actionPerformed(ActionEvent ae) {
-
-            Brd.Reset();
-            Player1Wins.setText("Player 1 Wins: "+ Brd.getPlayer1Score());
-            Player2Wins.setText("Player 2 Wins: "+ Brd.getPlayer2Score());
-
-        }
-
-    });
-
+    	add(Player1Wins);
+    	add(Player2Wins);
     
-    add(Player1Wins);
-    add(Player2Wins);
-    
-    add(Exit);
-    add(Reset);
-}
+    	add(Exit);
+    	add(Reset);
+	}
 
-public void SetObject(Board B) {
-
-    Brd = B;
-
-}
-
+	public void SetObject(Board B) {
+    		Brd = B;
+	}
 }
