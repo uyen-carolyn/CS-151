817d1e65a0cb5fc1d865b6dacdb5c66b770fb0b7
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-10-14 00:17:04 -0700

    Update Control.java

--- In Class Assignments/Control.java
+++ In Class Assignments/Control.java
@@ -1,34 +1,30 @@
 package tictactoe;
 
-
 import java.awt.*;
 import javax.swing.*;
 
 public class Control extends JFrame {
+    /*
+     this is the main controler that controls
+     and combines different components of the game.
+    */ 
 
-/*
-this is the main controler that controls
-and combines different components of the game.
-*/ 
+    private Board GameBoard; //Board and Button
+    private Tools TButtons; // Exit and Reset
 
-private Board GameBoard; //Board and Button
-private Tools TButtons; // Exit and Reset
+    Control() {
+        setLayout(new BorderLayout());
 
-Control() {
+        GameBoard = new Board();
+        TButtons = new Tools();
 
-    setLayout(new BorderLayout());
+        TButtons.SetObject(GameBoard);
 
-    GameBoard = new Board();
-    TButtons = new Tools();
+        add(GameBoard, BorderLayout.CENTER);
+        add(TButtons, BorderLayout.SOUTH);
 
-    TButtons.SetObject(GameBoard);
-
-    add(GameBoard, BorderLayout.CENTER);
-    add(TButtons, BorderLayout.SOUTH);
-
-    setVisible(true);
-    setSize(400, 300);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
-
+        setVisible(true);
+        setSize(400, 300); // altered dimensions to add in score for both players
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
