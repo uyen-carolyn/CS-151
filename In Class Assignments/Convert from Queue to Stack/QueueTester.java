157cd60619d47d4754438004732b93632843dd9a
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-11-13 23:32:09 -0800

    Create QueueTester.java

---
+++ In Class Assignments/Convert from Queue to Stack/QueueTester.java
@@ -0,0 +1,18 @@
+public class QueueTester
+{
+   public static void main(String[] args)
+   {
+      BoundedQueue<String> q = new BoundedQueue<>(10);
+
+      q.add("Belgium");
+      q.add("Italy");
+      q.add("France");
+      q.remove();
+      q.add("Thailand");
+
+      ArrayList<String> a = new ArrayList<>();
+      a.addAll(q);
+      System.out.println("Result of bulk add: " + a); 
+    System.out.println("Minimum: " + Collections.min(q));
+   }
+}
